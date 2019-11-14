1d5937923311c757be2f14f8c54cb147d98e550f
Fedor <fedykonovalov@gmail.com>
2019-11-14 10:23:07 +0300

    add anchors and yandex.metrics

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
 $(document).ready(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 1300) {
-      $('.page-up').fadeIn();
-    } else {
-      $('.page-up').fadeOut();
-    }
+  $("a[href]").click(function () {
+    var _href = $(this).attr("href");
+    $("html, body").animate({
+      scrollTop: $(_href).offset().top + "px"
+    }, 1500);
+    return false;
   });
-  // $("a[href=#up]").click(function () {
-  //   var _href = $(this).attr("href");
-  //   $("html, body").animate({
-  //     scrollTop: $(_href).offset().top + "px"
-  //   }, 1500);
-  //   return false;
-  // });
+  console.log('xuy1');
 });
