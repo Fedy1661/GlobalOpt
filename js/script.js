02fae3ec2043473c4d71ec13dce9ae1559c5630d
Fedor Konovalov <fedykonovalov@gmail.com>
2020-02-23 11:22:37 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $("a[href]").click(function () {
+    var _href = $(this).attr("href");
+    $("html, body").animate({
+      scrollTop: $(_href).offset().top + "px"
+    }, 1500);
+    return false;
+  });
+});
