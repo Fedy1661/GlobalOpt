2949c48c4421ec6a7f6c1e727ff719800408bca9
Fedor Konovalov <fedykonovalov@gmail.com>
2020-02-23 11:17:01 +0300

    True build

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $("a[href]").click(function () {
-    var _href = $(this).attr("href");
-    $("html, body").animate({
-      scrollTop: $(_href).offset().top + "px"
-    }, 1500);
-    return false;
-  });
-});
